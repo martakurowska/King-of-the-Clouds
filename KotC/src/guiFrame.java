1a1b638659e91c9b44c10a06edce660c2235b618
leukneys <0141349@pw.edu.pl>
2020-03-28 15:03:17 +0100

    nowe menu (teoretycznie)

--- KotC/src/guiFrame.java
+++ KotC/src/guiFrame.java
@@ -7,15 +7,22 @@ import javax.swing.*;
 import javax.imageio.ImageIO;
 import javax.sound.sampled.*;
 import java.net.URL;
+import java.util.ResourceBundle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-
-public class guiFrame extends JFrame{
+public class guiFrame extends JFrame  {
 	
 	Graphics g;
 	JPanel topPanel, centerPanel;                      
 	JLabel background;
-	JButton newGame, load, exit, level; 
-	ButtonGroup group; 
+	public JButton newGame, loadGame, exitGame, levelChange; 
+	private Font font; 
+	private Color color; 
+	public ResourceBundle bundle;
+/*	public LevelSettings levelSettings;
+	public ColorSettings colorSettings;					//tego jeszcze nie mamy 
+	public Game newgame; */
 	private ImageIcon menu;
 	Image img;
 	boolean started = false;
@@ -29,7 +36,11 @@ public class guiFrame extends JFrame{
 	//	this.setSize(1360, 960);
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		this.setTitle("King of the Clouds Game");
+		this.setFocusable(true);
+	    this.requestFocusInWindow();
 		this.setSize((int) (screenWidth*0.7),(int) (screenHeight*0.85) );
+		font = new Font("Impact", Font.PLAIN, 14);   								//ustawienia czcionki na przycisku 
+		color = new Color(225,108,164);												//ustawienie koloru przycisku
 		topPanel = new JPanel();
 		topPanel.setPreferredSize(new Dimension(700,50));
 		topPanel.setBackground( new Color(126,177,211));
@@ -46,7 +57,7 @@ public class guiFrame extends JFrame{
 		}
 		
 		centerPanel = new JPanel();
-		centerPanel.setBackground(null);
+/*		centerPanel.setBackground(null);
 		centerPanel.setOpaque(false);
 		centerPanel.setLayout(new GridLayout(0,1,0,80)); //rows, columns, size of horizontal gaps, size of vertical gaps
 		newGame = new JButton("New Game");
@@ -68,10 +79,52 @@ public class guiFrame extends JFrame{
         centerPanel.add(newGame);
         centerPanel.add(load);
         centerPanel.add(level);
-        centerPanel.add(exit);
+        centerPanel.add(exit); */
 		ImageIcon img = new ImageIcon(guiFrame.class.getResource("menu2.png"));
         background = new JLabel("",img,JLabel.CENTER);
         background.setBounds(0,0,(int) (screenWidth*0.7),(int) (screenHeight*0.85));
+        
+        add(background);
+        background.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.ipadx = 1;
+        c.insets = new Insets(10, 0, 0, 0);
+
+        c.gridx = 5;
+        
+        c.gridy = 0;
+        newGame = new JButton(bundle.getString("newgame"));
+        newGame.setFont(font);
+        newGame.setBackground(color);
+        newGame.setActionCommand("NewGame");
+ //     newGame.addActionListener(this);
+        background.add(newGame, c);
+        
+        c.gridy = 1;
+        loadGame = new JButton(bundle.getString("loadGame"));
+        loadGame.setFont(font);
+        loadGame.setBackground(color); 
+        loadGame.setActionCommand("loadGame");
+     // loadGame.addActionListener(this);
+        background.add(loadGame, c);
+
+        c.gridy = 2;
+        levelChange = new JButton(bundle.getString("levelChange"));
+        levelChange.setFont(font);
+        levelChange.setBackground(color);
+        levelChange.setActionCommand("levelChange");
+     // levelChange.addActionListener(this);
+        background.add(levelChange, c);
+
+        c.gridy = 3;
+        exitGame = new JButton(bundle.getString("exit"));
+        exitGame.setFont(font);
+        exitGame.setBackground(color); 
+        exitGame.setActionCommand("exitGame");
+     // exitGame.addActionListener(this);
+        background.add(exitGame, c);
+
         
        // background.setPreferredSize(new Dimension(500,600));
         this.add(background);
