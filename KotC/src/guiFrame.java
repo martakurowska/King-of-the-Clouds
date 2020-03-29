2d439c0107f92103166090cf7b75456e603cca7b
martakurowska <marta.kurowska1999@gmail.com>
2020-03-29 14:53:00 +0200

    FiFtH cOmMiT

--- KotC/src/guiFrame.java
+++ KotC/src/guiFrame.java
@@ -1,5 +1,4 @@
 import java.awt.*;
-import java.awt.Color; 
 import javax.swing.*;
 import javax.sound.sampled.*;
 import java.net.URL;
@@ -7,38 +6,108 @@ import java.util.ResourceBundle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class guiFrame extends JFrame implements ActionListener {
-	
-	Graphics g;
-	JPanel topPanel, topPanelA, topPanelB, centerPanel;                      
-	JLabel background;
-	public JButton newGame, loadGame, exitGame, settings, levelChange; 
-	private Font font; 
-	private Color color; 
-	ImageIcon img, img2;
+public class guiFrame extends JFrame {
+	                    
 	//public ResourceBundle bundle;
-	private String selected;
 /*	public LevelSettings levelSettings;
 	public ColorSettings colorSettings;					//tego jeszcze nie mamy 
 	public Game newgame; */
-	boolean started = false;
+	ImageIcon img;
+	Container con; 
+	private Font font; 
+	private Color color; 
+	public JButton newGameBtn, loadGameBtn, exitGameBtn, settingsBtn, levelChangeBtn; 
 	protected int screenWidth  = Toolkit.getDefaultToolkit().getScreenSize().width;
     protected int screenHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
-    public settingsFrame settingsFrame;
+    settingsFrame settings;
+	JPanel menuPanel, settingsPanel;
+	menuScreenHandler msHandler = new menuScreenHandler();
+
+	public JPanel menuFrame() {
+		
+		menuPanel = new JPanel() {
+			@Override
+		    public void paintComponent(Graphics G) {
+		        super.paintComponent(G);
+		        img = new ImageIcon(getClass().getResource("others/menu2.png"));
+		        G.drawImage(img.getImage(), 0, 0, this.getWidth(), this.getHeight(), null);
+		    }
+		};
+		menuPanel.setBounds(0,0,1360, 960);
+        
+		menuPanel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.ipadx = 150;
+        c.ipady = 15;
+        c.insets = new Insets(15, 0, 0, 0);
+
+        c.gridx = 0;
+        c.gridy = 0;
+        JLabel lbl1 = new JLabel("");
+        lbl1.setPreferredSize(new Dimension(10,140));
+        menuPanel.add(lbl1, c);
+        
+        c.gridy = 1;
+    	font = new Font("Impact", Font.PLAIN, 34);   								//ustawienia czcionki na przycisku 
+		color = new Color(225,108,164);												//ustawienie koloru przycisku
+        newGameBtn = new JButton("new game");
+        newGameBtn.setFont(font);
+        newGameBtn.setBackground(color);
+        newGameBtn.setForeground(Color.BLACK);
+        newGameBtn.setActionCommand("newgame");
+        newGameBtn.addActionListener(msHandler);
+        menuPanel.add(newGameBtn, c);
+
+        c.gridy = 2;
+        loadGameBtn = new JButton("load game");
+        loadGameBtn.setFont(font);
+        loadGameBtn.setBackground(color); 
+        loadGameBtn.setForeground(Color.BLACK);
+        loadGameBtn.setActionCommand("loadgame");
+        loadGameBtn.addActionListener(msHandler);
+        menuPanel.add(loadGameBtn, c);
+
+        c.gridy = 3;
+        levelChangeBtn = new JButton("difficulty");
+        levelChangeBtn.setFont(font);
+        levelChangeBtn.setBackground(color);
+        levelChangeBtn.setForeground(Color.BLACK);
+        levelChangeBtn.setActionCommand("difficulty");
+        levelChangeBtn.addActionListener(msHandler);
+        menuPanel.add(levelChangeBtn, c);
+
+        c.gridy = 4;
+        settingsBtn = new JButton("settings");
+        settingsBtn.setFont(font);
+        settingsBtn.setBackground(color); 
+        settingsBtn.setForeground(Color.BLACK);
+        settingsBtn.setActionCommand("settings");
+        settingsBtn.addActionListener(msHandler);
+        menuPanel.add(settingsBtn, c);
+        
+        c.gridy = 5;
+        exitGameBtn = new JButton("exit");
+        exitGameBtn.setFont(font);
+        exitGameBtn.setBackground(color); 
+        exitGameBtn.setForeground(Color.BLACK);
+        exitGameBtn.setActionCommand("exitgame");
+        exitGameBtn.addActionListener(msHandler);
+        menuPanel.add(exitGameBtn, c);
+		
+        return menuPanel;
+	}
 	
-    
-    
-    
 	public guiFrame() {
-		//this.setSize(1360, 960);
 		this.setSize((int) (screenWidth*0.7),(int) (screenHeight*0.85) );
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		this.setTitle("King of the Clouds Game");
 		this.setFocusable(true);
 	    this.requestFocusInWindow();
+		con = this.getContentPane();
 		
 		try {
-		    URL url = getClass().getResource("others/cgf.wav");
+		    URL url = getClass().getResource("others/KOTC.wav");
 		    AudioInputStream audio = AudioSystem.getAudioInputStream(url);
 		    Clip music = AudioSystem.getClip();
 		    music.open(audio);
@@ -47,107 +116,37 @@ public class guiFrame extends JFrame implements ActionListener {
 		catch (Exception e) {
 		}
 		
-		img = new ImageIcon(getClass().getResource("others/menu3.png"));
-        background = new JLabel("",img,JLabel.CENTER);
-        //background.setBounds(0,0,1360, 960);
-        //background.setBounds(0,0,(int) (screenWidth*0.7),(int) (screenHeight*0.85));
-        
-        background.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.ipadx = 150;
-        c.ipady = 18;
-        c.insets = new Insets(15, 0, 0, 0);
-
-        c.gridx = 0;
-        c.gridy = 0;
-        JLabel lbl1 = new JLabel("");
-        lbl1.setPreferredSize(new Dimension(10,140));
-        background.add(lbl1, c);
-        
-        c.gridy = 1;
-    	font = new Font("Impact", Font.PLAIN, 34);   								//ustawienia czcionki na przycisku 
-		color = new Color(225,108,164);												//ustawienie koloru przycisku
-        newGame = new JButton("new game");
-        newGame.setFont(font);
-        newGame.setBackground(color);
-        newGame.setForeground(Color.BLACK);
-        newGame.setActionCommand("newgame");
- //     newGame.addActionListener(this);
-        background.add(newGame, c);
-
-        c.gridy = 2;
-        loadGame = new JButton("load game");
-        loadGame.setFont(font);
-        loadGame.setBackground(color); 
-        loadGame.setForeground(Color.BLACK);
-        loadGame.setActionCommand("loadgame");
-     // loadGame.addActionListener(this);
-        background.add(loadGame, c);
-
-        c.gridy = 3;
-        levelChange = new JButton("difficulty");
-        levelChange.setFont(font);
-        levelChange.setBackground(color);
-        levelChange.setForeground(Color.BLACK);
-        levelChange.setActionCommand("difficulty");
-     // levelChange.addActionListener(this);
-        background.add(levelChange, c);
-
-        c.gridy = 4;
-        settings = new JButton("settings");
-        settings.setFont(font);
-        settings.setBackground(color); 
-        settings.setForeground(Color.BLACK);
-        settings.setActionCommand("settings");
-        settings.addActionListener(this);
-        background.add(settings, c);
-        
-        c.gridy = 5;
-        exitGame = new JButton("exit");
-        exitGame.setFont(font);
-        exitGame.setBackground(color); 
-        exitGame.setForeground(Color.BLACK);
-        exitGame.setActionCommand("exitgame");
-        exitGame.addActionListener(this);
-        background.add(exitGame, c);
-
-        
-       // background.setPreferredSize(new Dimension(500,600));
-        this.add(background);
+		menuPanel = new JPanel();
+		menuPanel = this.menuFrame(); 
+        con.add(menuPanel);
+       
 		
 		
-        
-    /*    this.add(leftPanel, BorderLayout.LINE_START);
-        this.add(rightPanel, BorderLayout.LINE_END); */ 
-        
-	
 	}
 	
-	@Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getActionCommand() == "newgame") {
-            selected = "newgame";
-        } 
-        else if (e.getActionCommand() == "loadgame") {
-            selected = "loadgame";
-        } 
-        else if (e.getActionCommand() == "difficulty") {
-            selected = "difficulty";
-        }
-        else if (e.getActionCommand() == "settings") {
-        	selected = "settings";
-        	//settingsFrame = new settingsFrame(this);
-        	//settingsFrame.setVisible(true);
-            //img2 = new ImageIcon(getClass().getResource("others/settings.png"));
-    		//background = new JLabel("",img2,JLabel.CENTER);
-    		
-    		
-        }
-        else if (e.getActionCommand() == "exitgame") {
-            selected = "exitgame";
-            System.exit(0);
-        }
-    }
-
+	public class menuScreenHandler implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if (e.getActionCommand() == "newgame") {
+				//setFalse(isMenuActive);
+				//setFalse(isSettingsActive);
+	        } 
+	        else if (e.getActionCommand() == "loadgame") {
+	        	//setFalse(isMenuActive);
+	        	//setFalse(isSettingsActive);
+	        } 
+	        else if (e.getActionCommand() == "difficulty") {
+	        	//setFalse(isSettingsActive);
+	        }
+	        else if (e.getActionCommand() == "settings") {
+	        	menuPanel.setVisible(false);
+	        	settings = new settingsFrame();
+	        	settingsPanel = settings.setUpSettings();
+	        	con.add(settingsPanel);
+	        }
+	        else if (e.getActionCommand() == "exitgame") {
+	            System.exit(0);
+	        }
+			
+		}
+	}
 }
